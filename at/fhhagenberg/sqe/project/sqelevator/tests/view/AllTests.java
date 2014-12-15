7a74a25d60e9e436c9c9c7a1d6c048dbfe5a92d0
Bernhard Raab <bernhard_raab@gmx.at>
2014-12-15 17:42:16 +0100

    changed absolut path in buildpath, added test files

---
+++ at/fhhagenberg/sqe/project/sqelevator/tests/view/AllTests.java
@@ -0,0 +1,8 @@
+package at.fhhagenberg.sqe.project.sqelevator.tests.view;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
---
+++ at/fhhagenberg/sqe/project/sqelevator/tests/view/ElevatorStatusTest.java
@@ -0,0 +1,24 @@
+package at.fhhagenberg.sqe.project.sqelevator.tests.view;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ElevatorStatusTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
