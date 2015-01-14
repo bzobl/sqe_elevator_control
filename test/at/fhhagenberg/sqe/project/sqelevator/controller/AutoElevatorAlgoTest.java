d4c681e610da9c61cefe129376279a984a2d8694
Bernhard Raab <bernhard_raab@gmx.at>
2015-01-14 13:36:37 +0100

    added more unit tests for autoAlgorithm

--- test/at/fhhagenberg/sqe/project/sqelevator/controller/AutoElevatorAlgoTest.java
+++ test/at/fhhagenberg/sqe/project/sqelevator/controller/AutoElevatorAlgoTest.java
@@ -5,7 +5,7 @@
  */
 package at.fhhagenberg.sqe.project.sqelevator.controller;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -183,5 +183,193 @@ public class AutoElevatorAlgoTest {
 		assertEquals(0,  mShunt.SetTarget);
 		((ElevatorTestWrapper)mSys.getElevator(0)).setFloor(0);	// change model
 		((ElevatorTestWrapper)mSys.getElevator(0)).setTargetFloor(0);
+	}
+	
+	
+	@Test
+	public void test1ElevatorAlreadyInFloorFloorRequest() throws FloorException, ElevatorException {
+		/* test floor request of moving elevator
+		 */
+		// setup
+		final int NUM_ELEVATORS = 1;
+		final int NUM_FLOORS = 5;
+		IElevatorConnection status = new SimpleElevatorSimulator(NUM_ELEVATORS, NUM_FLOORS);
+		mSys = new ElevatorSystemTestWrapper(status);
+		mShunt = new ElevatorConnectionTestShunt(NUM_FLOORS, HEIGHT, CAPACITY);
+		mAutoAlgo = new AutoElevatorAlgorithm(mSys, mShunt);
+		
+		// test
+		mShunt.CommitedDirection = IElevator.ELEVATOR_DIRECTION_UNCOMMITTED;
+		mShunt.Floor = 0;
+		mAutoAlgo.enableElevator(0, true);
+		
+		// request lowest floor
+		mAutoAlgo.setFloorRequest(0, true);
+		assertEquals(IElevator.ELEVATOR_DIRECTION_UNCOMMITTED, mShunt.CommitedDirection);
+		assertEquals(0,  mShunt.SetTarget);
+		((ElevatorTestWrapper)mSys.getElevator(0)).setTargetFloor(0);	// change model
+		
+		((ElevatorTestWrapper)mSys.getElevator(0)).setFloor(4);	
+		mShunt.SetTarget = 4;
+		
+		// request highest floor
+		mAutoAlgo.setFloorRequest(4, true);
+		assertEquals(IElevator.ELEVATOR_DIRECTION_UNCOMMITTED, mShunt.CommitedDirection);
+		assertEquals(4,  mShunt.SetTarget);
+	}
+	
+	
+	@Test
+	public void test1ElevatorDisabledRequest() throws FloorException, ElevatorException {
+		// setup
+		final int NUM_ELEVATORS = 1;
+		final int NUM_FLOORS = 5;
+		IElevatorConnection status = new SimpleElevatorSimulator(NUM_ELEVATORS, NUM_FLOORS);
+		mSys = new ElevatorSystemTestWrapper(status);
+		mShunt = new ElevatorConnectionTestShunt(NUM_FLOORS, HEIGHT, CAPACITY);
+		mAutoAlgo = new AutoElevatorAlgorithm(mSys, mShunt);
+		
+		// test
+		mShunt.CommitedDirection = IElevator.ELEVATOR_DIRECTION_UNCOMMITTED;
+		mShunt.Floor = 0;
+		mAutoAlgo.enableElevator(0, false);
+		
+		try {
+			mAutoAlgo.setElevatorRequest(0, 0);
+			fail();
+		} catch (AssertionError e) {
+			
+		}
+		assertEquals(IElevator.ELEVATOR_DIRECTION_UNCOMMITTED, mShunt.CommitedDirection);
+		assertEquals(0, mShunt.SetTarget);
+		
+		try {
+			mAutoAlgo.setElevatorRequest(0, 4);
+			fail();
+		} catch (AssertionError e) {
+			
+		}			
+		assertEquals(IElevator.ELEVATOR_DIRECTION_UNCOMMITTED, mShunt.CommitedDirection);
+		assertEquals(0, mShunt.SetTarget);
+	}
+	
+	
+	@Test
+	public void test1ElevatorDisabledFloorRequest() throws FloorException, ElevatorException {
+		/* test floor request of moving elevator
+		 */
+		// setup
+		final int NUM_ELEVATORS = 1;
+		final int NUM_FLOORS = 5;
+		IElevatorConnection status = new SimpleElevatorSimulator(NUM_ELEVATORS, NUM_FLOORS);
+		mSys = new ElevatorSystemTestWrapper(status);
+		mShunt = new ElevatorConnectionTestShunt(NUM_FLOORS, HEIGHT, CAPACITY);
+		mAutoAlgo = new AutoElevatorAlgorithm(mSys, mShunt);
+		
+		// test
+		mShunt.CommitedDirection = IElevator.ELEVATOR_DIRECTION_UNCOMMITTED;
+		mShunt.Floor = 0;
+		mAutoAlgo.enableElevator(0, false);
+		
+		// request lowest floor
+		mAutoAlgo.setFloorRequest(0, true);
+		assertEquals(IElevator.ELEVATOR_DIRECTION_UNCOMMITTED, mShunt.CommitedDirection);
+		assertEquals(0,  mShunt.SetTarget);
+		
+		mAutoAlgo.setFloorRequest(0, false);
+		assertEquals(IElevator.ELEVATOR_DIRECTION_UNCOMMITTED, mShunt.CommitedDirection);
+		assertEquals(0,  mShunt.SetTarget);
+		
+		// request highest floor
+		mAutoAlgo.setFloorRequest(4, true);
+		assertEquals(IElevator.ELEVATOR_DIRECTION_UNCOMMITTED, mShunt.CommitedDirection);
+		assertEquals(0,  mShunt.SetTarget);
+		
+		mAutoAlgo.setFloorRequest(4, false);
+		assertEquals(IElevator.ELEVATOR_DIRECTION_UNCOMMITTED, mShunt.CommitedDirection);
+		assertEquals(0,  mShunt.SetTarget);
+	}
+	
+	
+	@Test
+	public void test1ElevatorFloorNotServicedRequest() throws FloorException, ElevatorException {
+		// setup
+		final int NUM_ELEVATORS = 1;
+		final int NUM_FLOORS = 5;
+		IElevatorConnection status = new SimpleElevatorSimulator(NUM_ELEVATORS, NUM_FLOORS);
+		mSys = new ElevatorSystemTestWrapper(status);
+		mShunt = new ElevatorConnectionTestShunt(NUM_FLOORS, HEIGHT, CAPACITY);
+		mAutoAlgo = new AutoElevatorAlgorithm(mSys, mShunt);
+		
+		// test
+		mShunt.CommitedDirection = IElevator.ELEVATOR_DIRECTION_UNCOMMITTED;
+		mShunt.Floor = 0;
+		mAutoAlgo.enableElevator(0, true);
+		mShunt.setServicesFloors(0, 4, false);
+		((ElevatorTestWrapper)mSys.getElevator(0)).setServicesFloors(4, false);
+		
+		((ElevatorTestWrapper)mSys.getElevator(0)).setButtonStatus(1, true);
+		mAutoAlgo.setElevatorRequest(0, 1);
+		assertEquals(IElevator.ELEVATOR_DIRECTION_UP, mShunt.CommitedDirection);
+		assertEquals(1, mShunt.SetTarget);
+		
+		((ElevatorTestWrapper)mSys.getElevator(0)).setFloor(1);	// change model
+		((ElevatorTestWrapper)mSys.getElevator(0)).setTargetFloor(1);		
+		
+		// floor 4 is not serviced
+		mAutoAlgo.setElevatorRequest(0, 4);
+		assertEquals(IElevator.ELEVATOR_DIRECTION_UNCOMMITTED, mShunt.CommitedDirection);
+		assertEquals(1, mShunt.SetTarget);
+	}
+	
+	
+	@Test
+	public void test1ElevatorFloorNotServicedFloorRequest() throws FloorException, ElevatorException {
+		/* test floor request of moving elevator
+		 */
+		// setup
+		final int NUM_ELEVATORS = 1;
+		final int NUM_FLOORS = 5;
+		IElevatorConnection status = new SimpleElevatorSimulator(NUM_ELEVATORS, NUM_FLOORS);
+		mSys = new ElevatorSystemTestWrapper(status);
+		mShunt = new ElevatorConnectionTestShunt(NUM_FLOORS, HEIGHT, CAPACITY);
+		mAutoAlgo = new AutoElevatorAlgorithm(mSys, mShunt);
+		
+		// test
+		mShunt.CommitedDirection = IElevator.ELEVATOR_DIRECTION_UNCOMMITTED;
+		mShunt.Floor = 0;
+		mAutoAlgo.enableElevator(0, true);
+		mShunt.setServicesFloors(0, 2, false);
+		mShunt.setServicesFloors(0, 4, false);
+		((ElevatorTestWrapper)mSys.getElevator(0)).setServicesFloors(2, false);
+		((ElevatorTestWrapper)mSys.getElevator(0)).setServicesFloors(4, false);
+		
+		// request floor 1
+		mAutoAlgo.setFloorRequest(1, true);
+		assertEquals(IElevator.ELEVATOR_DIRECTION_UP, mShunt.CommitedDirection);
+		assertEquals(1,  mShunt.SetTarget);
+		
+		((ElevatorTestWrapper)mSys.getElevator(0)).setFloor(1);	
+		mShunt.CommitedDirection = IElevator.ELEVATOR_DIRECTION_UNCOMMITTED;
+		mShunt.SetTarget = 1;
+		
+		// floor 2 is not serviced
+		mAutoAlgo.setFloorRequest(2, false);
+		assertEquals(IElevator.ELEVATOR_DIRECTION_UNCOMMITTED, mShunt.CommitedDirection);
+		assertEquals(1,  mShunt.SetTarget);
+		
+		// request floor 3
+		mAutoAlgo.setFloorRequest(3, true);
+		assertEquals(IElevator.ELEVATOR_DIRECTION_UP, mShunt.CommitedDirection);
+		assertEquals(3,  mShunt.SetTarget);		
+		
+		((ElevatorTestWrapper)mSys.getElevator(0)).setFloor(3);	
+		mShunt.CommitedDirection = IElevator.ELEVATOR_DIRECTION_UNCOMMITTED;
+		mShunt.SetTarget = 3;
+		
+		// floor 4 is not serviced
+		mAutoAlgo.setFloorRequest(4, false);
+		assertEquals(IElevator.ELEVATOR_DIRECTION_UNCOMMITTED, mShunt.CommitedDirection);
+		assertEquals(3,  mShunt.SetTarget);
 	}
 }
